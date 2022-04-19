27568e1508a0518072b33a9f83ffd5729d71112c
DESKTOP-53F5J9A\User <seungeonjoo>
2022-04-19 12:09:29 +0900

    ㄴ

--- packages/usp/src/pages/Index/Index.tsx
+++ packages/usp/src/pages/Index/Index.tsx
@@ -1,24 +1,205 @@
 import { NavLink } from 'react-router-dom';
 import * as styles from './styles';
-
-const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 
 function Home() {
+
   return (
     <div css={styles.container}>
-      <div>
-        <NavLink to="/factor">비밀번호 확인 페이지</NavLink>
+      <div className='content'>
+        <Typography variant="h3" gutterBottom component="div">
+          화면 컴포넌트
+        </Typography>
+        <TableContainer component={Paper}>
+          <Table sx={{ width: "100%" }}aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>섹션</TableCell>
+                <TableCell align="left">1 뎁스</TableCell>
+                <TableCell align="left">2 뎁스</TableCell>
+                <TableCell align="left">화면 ID</TableCell>
+                <TableCell align="left">화면 URL</TableCell>
+                <TableCell align="left">기타</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row">메인 홈</TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left">UI-USP-FRN-0370101</TableCell>
+                  <TableCell align="left"><NavLink to="/Home">/Home</NavLink></TableCell>
+                  <TableCell align="left"></TableCell>
+                </TableRow>
+                {/* 로그인, 회원가입 */}
+                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row">회원가입</TableCell>
+                  <TableCell align="left">회원유형 선택</TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left">UI-USP-FRN-0010101</TableCell>
+                  <TableCell align="left"><NavLink to="/SignUp">/SignUp</NavLink></TableCell>
+                  <TableCell align="left"></TableCell>
+                </TableRow>
+                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row">약관동의</TableCell>
+                  <TableCell align="left">사업자 (팝업포함)</TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left">UI-USP-FRN-0011501</TableCell>
+                  <TableCell align="left"><NavLink to="/SignUp/Consumer">/SignUp/Consumer</NavLink></TableCell>
+                  <TableCell align="left"></TableCell>
+                </TableRow>
+                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row"></TableCell>
+                  <TableCell align="left">보호자 휴대폰인증</TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left">UI-USP-FRN-0010901</TableCell>
+                  <TableCell align="left"><NavLink to="/SignUp/Confirm">/SignUp/Confirm</NavLink></TableCell>
+                  <TableCell align="left"></TableCell>
+                </TableRow>
+                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row"></TableCell>
+                  <TableCell align="left">기가입안내(사업자)</TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left">UI-USP-FRN-0011701</TableCell>
+                  <TableCell align="left"><NavLink to="/SignUp/Exist">/SignUp/Exist</NavLink></TableCell>
+                  <TableCell align="left"></TableCell>
+                </TableRow>
+                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row"></TableCell>
+                  <TableCell align="left">가입정보 입력(개인)</TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left">UI-USP-FRN-0010501</TableCell>
+                  <TableCell align="left"><NavLink to="/">/</NavLink></TableCell>
+                  <TableCell align="left"></TableCell>
+                </TableRow>
+                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row"></TableCell>
+                  <TableCell align="left">가입정보 입력(사업자)</TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left">UI-USP-FRN-0010601</TableCell>
+                  <TableCell align="left"><NavLink to="/">/</NavLink></TableCell>
+                  <TableCell align="left"></TableCell>
+                </TableRow>
+                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row"></TableCell>
+                  <TableCell align="left">가입완료</TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left">UI-USP-FRN-0011801</TableCell>
+                  <TableCell align="left"><NavLink to="/">/</NavLink></TableCell>
+                  <TableCell align="left"></TableCell>
+                </TableRow>
+                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row">로그인</TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left">UI-USP-FRN-0020101</TableCell>
+                  <TableCell align="left"><NavLink to="/SignIn">/SignIn</NavLink></TableCell>
+                  <TableCell align="left"></TableCell>
+                </TableRow>
+                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row">아이디 찾기</TableCell>
+                  <TableCell align="left">본인확인정보입력</TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left">UI-USP-FRN-0030201</TableCell>
+                  <TableCell align="left"><NavLink to="/">/</NavLink></TableCell>
+                  <TableCell align="left"></TableCell>
+                </TableRow>
+                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row"></TableCell>
+                  <TableCell align="left">아이디 확인</TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left">UI-USP-FRN-0030301</TableCell>
+                  <TableCell align="left"><NavLink to="/">/</NavLink></TableCell>
+                  <TableCell align="left"></TableCell>
+                </TableRow>
+                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row">비밀번호재설정</TableCell>
+                  <TableCell align="left">본인확인정보입력</TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left">UI-USP-FRN-0040201</TableCell>
+                  <TableCell align="left"><NavLink to="/">/</NavLink></TableCell>
+                  <TableCell align="left"></TableCell>
+                </TableRow>
+                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row"></TableCell>
+                  <TableCell align="left">본인인증</TableCell>
+                  <TableCell align="left">휴대폰인증</TableCell>
+                  <TableCell align="left">UI-USP-FRN-0040301</TableCell>
+                  <TableCell align="left"><NavLink to="/">/</NavLink></TableCell>
+                  <TableCell align="left"></TableCell>
+                </TableRow>
+                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row"></TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left">이메일인증</TableCell>
+                  <TableCell align="left">UI-USP-FRN-0040301</TableCell>
+                  <TableCell align="left"><NavLink to="/">/</NavLink></TableCell>
+                  <TableCell align="left"></TableCell>
+                </TableRow>
+                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row"></TableCell>
+                  <TableCell align="left">비밀번호 재설정</TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left">UI-USP-FRN-0040401</TableCell>
+                  <TableCell align="left"><NavLink to="/">/</NavLink></TableCell>
+                  <TableCell align="left"></TableCell>
+                </TableRow>
+                {/* 회원정보 변경 */}
+                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row">회원정보 변경</TableCell>
+                  <TableCell align="left">비밀번호 확인</TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left">UI-USP-FRN-0050101</TableCell>
+                  <TableCell align="left"><NavLink to="/Factor">/Factor</NavLink></TableCell>
+                  <TableCell align="left"></TableCell>
+                </TableRow>
+                {/* 서브메인: 모집공고 */}
+                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row">모집공고</TableCell>
+                  <TableCell align="left">목록 정시,상시 (탭처리)</TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left">UI-USP-FRN-0110101</TableCell>
+                  <TableCell align="left"><NavLink to="/a1/Notice">/Notice</NavLink></TableCell>
+                  <TableCell align="left"></TableCell>
+                </TableRow>
+                {/* 마이페이지 */}
+                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row">마이페이지</TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left">UI-USP-FRN-0110101</TableCell>
+                  <TableCell align="left"><NavLink to="/mypage">/mypage</NavLink></TableCell>
+                  <TableCell align="left"></TableCell>
+                </TableRow>
+                {/* 샘플 */}
+                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row">게시판 샘플</TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left">UI-USP-FRN-0370101</TableCell>
+                  <TableCell align="left"><NavLink to="/board">/board</NavLink></TableCell>
+                  <TableCell align="left"></TableCell>
+                </TableRow>
+                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row">컴포넌트 샘플</TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left"><NavLink to="/composampl">/composampl</NavLink></TableCell>
+                  <TableCell align="left"></TableCell>
+                </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
-      <div>
-        <NavLink to="/board">게시판 샘플</NavLink>
-      </div>
-      <div>
-        <NavLink to="/composampl">컴포넌트 샘플</NavLink>
-      </div>
-      <div>
-        <NavLink to="/mypage">마이페이지</NavLink>
-      </div>
-      <div style={{ height: 900 }}></div>
     </div>
   );
 }
--- packages/usp/src/pages/Index/styles.ts
+++ packages/usp/src/pages/Index/styles.ts
@@ -1,7 +1,12 @@
 import { css } from '@emotion/react';
 
 export const container = css`
-  .blue{
+  z-index: 9999;
+  position: absolute;
+  width: 100%;
+  padding: 150px 100px;
+  background: #fff;
+  a {
     color: #4063EC;
   }
   .MuiTypography-root{
@@ -15,148 +20,3 @@ export const container = css`
   }
 `;
 
-export const maincont01 = css`
-  position: relative;
-  overflow: hidden;
-  display: block;
-  color: #fff;
-  .main_benner{
-    text-align: center;
-    background-color: #170E47;
-    height: 840px;
-    width: 100%;
-    > img {
-      position: absolute;
-      right: 50%;
-      transform: translateX(50%);
-      width: auto;
-      height: 840px;
-    }
-  }
-  .main_txtbox{
-    position: absolute;
-    max-width: 1080px;
-    width: 100%;
-    top: 270px;
-    left: 50%;
-    display: flex;
-    left: 50%;
-    transform: translateX(-50%);
-    .main_tit{
-      flex: 1;
-      font-size: 80px;
-      letter-spacing: -4px;
-      font-weight: 100;
-      strong {
-        font-weight: 800;
-      }
-    }
-    p {
-      flex: 1;    
-      position: absolute;
-      bottom: 0;
-      right: 0;
-      line-height: 1.8;
-    }
-  }
-  .swifebtn{
-    position: absolute;
-    bottom: 70px;
-    right: -70px;
-    background-color: #4063EC;
-    color: #fff;
-    border-radius: 100%;
-    line-height: 1.2;
-    font-size: 12px;
-    width: 80px;
-    height: 80px;
-  }
-`;
-
-
-export const maincont02 = css`
-  height: 840px;
-  background-color: #1F2437;
-  color: #fff;
-  .md_btn{
-    color: #fff;
-    border: 1px solid #fff;
-    width: 220px;
-    height: 55px;
-    border-radius: 0;
-    margin-top: 10px;
-  }
-`;
-
-export const hotslide = css`
-  display: flex;
-  background-color: rgba(0,0,0,0);
-  border-radius: 15px;
-  color: #fff;
-  width: 380px;
-  .sub_txt{
-    color: #707070;
-    line-height:1;
-    font-size: 14px;
-    margin: 7px 0;
-  }
-`;
-
-export const maincont03 = css`
-  position: relative;
-  height: 1331px;
-  background-color: #F5F5F5;
-  img.char01{
-    position: absolute;
-    top: 150px;
-  }
-`;
-export const maincont04 = css`
-  height: 840px;
-  background-color: #4063EC;
-  color: #fff;
-`;
-export const maincont05 = css`
-  height: 892px;
-  background-color: #1F2437;
-  color: #fff;
-`;
-
-export const btnstyle = css`
-button{
-  border-radius: 0;
-  font-weight: Bold;
-  font-size: 16px;
-  padding: 16px 36px;
-  letter-spacing: -0.4px;
-}
-button.lg{
-  min-width: 200px;
-}
-button.md{
-  border-radius: 4px;
-  font-weight: normal;
-  padding: 8px 16px;
-}
-.blue{
-  background-color: #4063EC;
-}
-.gray{
-  background-color: #ADAEB2;
-}
-.gray:hover{
-  background-color: #9B9B9B;
-}
-.sky{
-  background-color: #EBEFFD;
-  color: #4063EC;
-}
-.sky:hover{
-  background-color: #d4deff
-}
-`;
-export const link = css`
-  a{
-    color: blue;
-  }
-`;
